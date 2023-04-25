3db3f052682bde103265a058309d7ba521b9b6de
Johan Andersson <repi@repi.se>
2023-04-26 00:19:11 +0200

    Add initial Cargo app source install

---
+++ install.sh
@@ -0,0 +1,26 @@
+# cargo binaries installed by building from source
+cargo install \
+    cargo-auditable \
+    cargo-deny \
+    cargo-fuzz \
+    cargo-insta \
+    cargo-machete \
+    cargo-miri \
+    cargo-nextest \
+    cargo-supply-chain \
+    cargo-edit \
+    cargo-vet \
+    cargo-watch \
+    bat \
+    exa \
+    du-dust \
+    hyperfine \
+    jless \
+    mdbook \
+    mdbook-toc \
+    puffin_viewer \
+    rg \
+    tokei \
+    twiggy \
+    typos-cli 
+
