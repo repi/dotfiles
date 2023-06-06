f4c4e155663cab7e2fb93f112447a1232e225fa8
Johan Andersson <repi@repi.se>
2023-06-06 11:43:15 +0200

    Add aliases for `cargo-vet`

--- cargo.sh
+++ cargo.sh
@@ -10,5 +10,13 @@ alias cbr="cargo build --release"
 alias cnr="cargo nextest run"
 alias cnra="INSTA_FORCE_PASS=1 INSTA_UPDATE=always cargo nextest run"
 alias crr="cargo run --release"
+
 alias citr="cargo insta test --review"
 alias cita="cargo insta test --accept"
+
+alias cvt="cargo vet trust --criteria safe-to-deploy"
+alias cvs="cargo vet suggest"
+alias cvp="cargo vet prune && cargo vet"
+alias cv="cargo vet"
+
+alias cud="cargo update --dry-run"
