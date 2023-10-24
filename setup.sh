547dc6f9dec8166501538cd6a3054ba0fbe8c85f
Johan Andersson <repi@repi.se>
2023-10-24 16:27:48 +0200

    Add ~/.local/bin to PATH

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH="$HOME/.ark/bin:$HOME/.dotfiles/bin:$PATH"
+export PATH="$HOME/.ark/bin:$HOME/.dotfiles/bin:$HOME/.local/bin:$PATH"
 
 source $HOME/.dotfiles/cargo.sh
 source $HOME/.dotfiles/git.sh
